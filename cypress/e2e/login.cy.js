ffcd82419aba0a0c39d2f5caa63d95ced197e7ea
Matheus  Souza <meteuu@gmail.com>
2025-12-01 21:02:33 -0300

    Atualiza testes de login com beforeEach e data-testid

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,47 @@
 describe('Login', () => {
+
+    beforeEach(() => {
+    cy.visit('https://front.serverest.dev/admin/home')
+  })
+
+  // Login com email e senha cadastrado com Sucesso!!
   it('deve realizar login com sucesso', () => {
 
-    // Entrando no site
-    cy.visit('https://front.serverest.dev/admin/home');
-
     // Preenchendo o formulário de login
-    cy.get('[placeholder="Digite seu email"]').type('matheusrieper8@gmail.com');
-    cy.get('[placeholder="Digite sua senha"]').type('Emilly2015.');
+    cy.get('[data-testid="email"]').type('matheus@gmail.com')
+    cy.get('[data-testid="senha"]').type('12345678')
 
-    // Clicando no botão Entrar
-    cy.contains('button', 'Entrar').click();
+    cy.contains('button', 'Entrar').click()
 
     // Validando que a página home carregou corretamente
-    cy.get('h1').should('be.visible');
-  });
-});
+    cy.get('h1').should('be.visible')
+  })
+
+  // Email não Cadastrado!!
+  it('Não deve Logar com Email cadastrado', () => {
+
+    // Preenchendo o formulário de login
+    cy.get('[data-testid="email"]').type('matheu@gmail.com')
+    cy.get('[data-testid="senha"]').type('12345678')
+
+    cy.contains('button', 'Entrar').click()
+
+    // Verificando mensagem de Erro!!!
+    cy.contains('Email e/ou senha inválidos')
+      .should('be.visible')
+  })
+
+  // Senha invalida!!
+  it('Não deve Logar com Senha Inválida', () => {
+
+    // Preenchendo o formulário de login
+    cy.get('[data-testid="email"]').type('matheus@gmail.com')
+    cy.get('[data-testid="senha"]').type('1234567')
+
+    cy.contains('button', 'Entrar').click()
+
+    // Verificando mensagem de Erro!!!
+    cy.contains('Email e/ou senha inválidos')
+      .should('be.visible')
+  })
+})
